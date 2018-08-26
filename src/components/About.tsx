ec1e381c1fd91c271a397e59771b49feda112fce
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-08-27 01:18:10 +1200

    Add about n change color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
-const About = (props: any) => <div />;
+const About = (props: any) => (
+  <div id="page-container"
+    style={{
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
+    }}>
+    <h1>Author: Tien Nguyen Khac</h1>
+    <h2>Find more cool stuffs that I do here</h2>
+    <h3><a href="https://github.com/crazycat9x">GitHub</a> - <a href="https://www.linkedin.com/in/nktien/">Linkedin</a></h3>
+  </div>
+);
 
 export default About;
