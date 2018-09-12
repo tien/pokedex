e4721c1bd95958268bdc7fb7288eef88e345fae9
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-13 10:12:03 +1200

    Build

--- src/services/pokeService.ts
+++ src/services/pokeService.ts
@@ -1,21 +1,6 @@
 import regrest from "regrest";
 import pokemonsList from "../assets/pokemons.json";
 
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    "use strict";
-    if (typeof start !== "number") {
-      start = 0;
-    }
-
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
-  };
-}
-
 class PokeService {
   public static imageUrlBase: string =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
